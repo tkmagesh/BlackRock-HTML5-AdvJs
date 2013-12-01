fd8cea7857e6ffe655ab8edecd62745aedcf6f13
tkmagesh <tkmagesh77@gmail.com>
2013-12-01 12:36:30 -0800

    WebSockets, Geolocations, Canvas etc...

--- Day-4/HTML5/WebSocketsDemo/ChatServer/SocketServer.js
+++ Day-4/HTML5/WebSocketsDemo/ChatServer/SocketServer.js
@@ -1,6 +1,5 @@
 var ws = require("nodejs-websocket")
 
-// Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
     console.log("New connection")
     conn.on("text", function (str) {
